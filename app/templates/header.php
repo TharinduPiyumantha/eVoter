3c944ebc9d0d7c731bc2ee3856104f1806378b9e
cutedili <dweerasinghe91@gmail.com>
2016-06-06 14:02:29 +0530

    navigation bar customization

---
+++ app/templates/header.php
@@ -0,0 +1,41 @@
+<?php define( 'SCRIPT_ROOT', 'http://localhost/evoter' );?>
+
+<html>
+<meta charset="UTF-8">
+<title>eVoter</title>
+<head>
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo SCRIPT_ROOT ?>/public/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo SCRIPT_ROOT ?>/public/css/sb-admin.css" rel="stylesheet">
+    <link href="<?php echo SCRIPT_ROOT ?>/public/css/custom.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="<?php echo SCRIPT_ROOT ?>/public/css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo SCRIPT_ROOT ?>/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- bootstrap css -->
+    <!--<link href="css/bootstrap.css" rel="stylesheet" type="text/css">-->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery -->
+    <script src="<?php echo SCRIPT_ROOT ?>/public/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo SCRIPT_ROOT ?>/public/js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="<?php echo SCRIPT_ROOT ?>/public/js/plugins/morris/raphael.min.js"></script>
+    <script src="<?php echo SCRIPT_ROOT ?>/public/js/plugins/morris/morris.min.js"></script>
+    <script src="<?php echo SCRIPT_ROOT ?>/public/js/plugins/morris/morris-data.js"></script>
+
+
---
+++ app/templates/sidemenu.php
@@ -0,0 +1,30 @@
+<!-- Navigation -->
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav side-nav">
+            <li class="active">
+                <a href="index.php"><i class="fa fa-fw fa-dashboard"></i>Home Page</a>
+            </li>
+            <li>
+                <a href="charts.html"><i class="fa fa-fw fa-user"></i>Members</a>
+            </li>
+            <li>
+                <a href="tables.html"><i class="fa fa-fw fa-table"></i>News Feed</a>
+            </li>
+            <li>
+                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>Election Event<i class="fa fa-fw fa-caret-down"></i></a>
+                <ul id="demo" class="collapse">
+                    <li>
+                        <a href="#">Create Event</a>
+                    </li>
+                    <li>
+                        <a href="#">Add Members</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i>User Pages</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.navbar-collapse -->
