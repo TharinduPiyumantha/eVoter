c2ad0354866ea5177c803b699458985fb56b618f
cutedili <dweerasinghe91@gmail.com>
2016-06-06 17:12:43 +0530

    db connection

---
+++ app/model/dbConfig.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+$db_host="localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name="evoter";
+
+try {
+    $db_con = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
+    $db_con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+$user = new USER($db_con);
+
+?>
